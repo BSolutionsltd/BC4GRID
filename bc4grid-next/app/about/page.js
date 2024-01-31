0072f4e6174867e7dd1433efa298cfc6128134d4
incepton-lab <domagoj.severdija@incepton.hr>
2024-01-31 13:30:04 +0100

    about tipos fixed

--- bc4grid-next/app/about/page.js
+++ bc4grid-next/app/about/page.js
@@ -33,7 +33,7 @@ export default function AboutPage() {
       <Segment style={style.segment} >
       <Header as='h2'>Project</Header>
       <p>
-      The development of this application was supported by the EU H2020 SMART4ALL #3 CTTE "BC for creating decentralized smart grid system (BC4GRID)" project. BC4GRID aims to utilize Blockchain technology in the electricity sector as a crucial tool for transitioning to decentralized smart grids. The project's goal is to create Blockchain software for smart grids that can optimize existing processes, secure grid management, and enable energy trading. The result will be novel Blockchain software with enhanced trustworthiness and reliability in the smart grid sector, allowing energy producers, consumers, and prosumers, as well as all related stakeholders, to have timely, accurate, and reliable information on energy consumption, trading, and system maintenance.
+      The development of this application was supported by the <strong>EU H2020 SMART4ALL #3 CTTE BC for creating decentralized smart grid system (BC4GRID) </strong> project. BC4GRID aims to utilize Blockchain technology in the electricity sector as a crucial tool for transitioning to decentralized smart grids. The projects goal is to create Blockchain software for smart grids that can optimize existing processes, secure grid management, and enable energy trading. The result will be novel Blockchain software with enhanced trustworthiness and reliability in the smart grid sector, allowing energy producers, consumers, and prosumers, as well as all related stakeholders, to have timely, accurate, and reliable information on energy consumption, trading, and system maintenance.
       </p>
       <p>
 BC4GRID has a significant impact on sensitive social groups in two ways:
